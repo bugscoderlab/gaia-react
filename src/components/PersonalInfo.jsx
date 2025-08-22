ddbcd91ae125794705d182693d40662ab154a83f
bugscoder-github <73679325+bugscoder-github@users.noreply.github.com>
2025-08-22 13:59:50 +0800

    moved PersonalInfo.jsx into components folder

---
+++ src/components/PersonalInfo.jsx
@@ -0,0 +1,21 @@
+function PersonalInfo() {
+    const currentDateTime = new Date().toLocaleString();
+
+    return (<>
+        <h1>Nama aku</h1>
+        <p>ini react</p>
+        <p>Current date and time: {currentDateTime}</p>
+        <img 
+            src="https://picsum.photos/300/200" alt="Random image"
+            style={{ width: '300px', height: '200px', borderRadius: '8px' }}
+        />
+        <p>my hobbies are:</p>
+        <ul>
+            <li>main</li>
+            <li>makan</li>
+            <li>minum</li>
+        </ul>
+    </>)
+}
+
+export default PersonalInfo;
