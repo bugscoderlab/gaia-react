e1e008b475b0d6f895b56ba9c894343d1ffc19bb
bugscoder-github <73679325+bugscoder-github@users.noreply.github.com>
2025-08-29 16:04:36 +0800

    apifetch as hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,49 @@
 import { useEffect, useState } from "react";
 
-const fetchAPI = async () => {
-  try {
-    const response = await fetch("https://dogapi.dog/api/v2/breeds", {
-      method: "GET",
-      // body: JSON.stringify(""),
-      headers: {
-        "Content-Type": "application/json"
-      },
-    })
+const useFetch = (url) => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
-    // console.log("Response: ", response);
-    return response.json()
-  } catch (err) {
-    return err;
-  }
-}
-
-function App() {
-  const [data, setData] = useState({
-    data: []
-  });
+  const fetchData = async () => {
+    try {
+      setData([]);
+      setLoading(true);
+      const response = await fetch(url);
+      const result = await response.json();
+      setData(result.data);
+    } catch (err) {
+      setError(err)
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    console.log("fetching");
+    fetchData()
+  }, []);
 
-    (async() => {
-      try {
-        const result = await fetchAPI();
-        setData(result);
-        // console.log(result);
-      } catch (e) {
-        console.log(e)
-      }
-    })();
-  }, [])
+  return { data, loading, error, fetchData };
+};
 
-  return(<>
-    <ul>
-    {data.data.map((val, key) => (
-      <li key={key}>{val.attributes.name}</li>
-    ))}
-    </ul>
-  </>)
+function App() {
+  const { data, loading, error, fetchData } = useFetch("https://dogapi.dog/api/v2/breeds");
+
+  return(
+    <>
+      <button onClick={fetchData} disabled={loading}>
+        {loading ? 'Loading...' : 'Fetch Data'}
+      </button>
+
+      {error && <p>Error: {error}</p>}
+      
+      <ul>
+        {data.map((val, key) => (
+          <li key={key}>{val.attributes.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
